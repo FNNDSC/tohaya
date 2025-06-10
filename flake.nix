b45512c6c78038c94133bc90d4926aad0f535b05
Jennings Zhang <dev@sl.jennin.xyz>
2025-06-10 17:32:28 -0400

    nix flake

---
+++ flake.nix
@@ -0,0 +1,27 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, flake-utils, nixpkgs }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = (import nixpkgs) {
+          inherit system;
+        };
+      in
+      with pkgs;
+      {
+        packages.default = rustPlatform.buildRustPackage {
+          pname = "tohaya";
+          version = self.shortRev or self.dirtyShortRev;
+          src = ./.;
+          cargoLock = {
+            lockFile = ./Cargo.lock;
+          };
+          nativeBuildInputs = [ pkg-config ];
+          doCheck = false;
+        };
+      });
+}
